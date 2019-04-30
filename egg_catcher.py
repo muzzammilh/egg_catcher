5abd7258472c1a50f72d00e5af52b385b273d2df
Muzzammil Hussain <muzzammilz@gmail.com>
2019-04-30 17:21:53 +0500

    Our sun.

--- egg_catcher.py
+++ egg_catcher.py
@@ -7,6 +7,7 @@ CANVAS_WIDTH = 800
 CANVAS_HEIGHT = 400
 CANVAS_BACKGROUND = 'deep sky blue'
 GRASS_COLOR = 'sea green'
+SUN_COLOR = 'orange'
 
 # Create a window.
 root = Tk()
@@ -15,7 +16,10 @@ root = Tk()
 c = Canvas(root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT, background=CANVAS_BACKGROUND)
 
 # Create some grass on the canvas bottom
-c.create_rectangle(-5, CANVAS_HEIGHT - 100, CANVAS_WIDTH + 5, CANVAS_HEIGHT + 5, fill=GRASS_COLOR, width=0 )
+c.create_rectangle(-5, CANVAS_HEIGHT - 100, CANVAS_WIDTH + 5, CANVAS_HEIGHT + 5, fill=GRASS_COLOR, width=0)
+
+# Create a sun in top left corner
+c.create_oval(-80, -80, 120, 120, fill=SUN_COLOR, width=0)
 
 # Combine everything and draw
 c.pack()
